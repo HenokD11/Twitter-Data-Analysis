866e675157b673420a76b1c80e845bb8a7b4b470
ahenokD1 <95631406+ahenokD1@users.noreply.github.com>
2022-08-09 19:02:09 +0300

    clean tweets dataframe fixed

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -4,6 +4,7 @@ class Clean_Tweets:
     The PEP8 Standard AMAZING!!!
     """
     def __init__(self, df:pd.DataFrame):
+
         self.df = df
         print('Automation in Action...!!!')
         
@@ -16,22 +17,23 @@ class Clean_Tweets:
         df.drop(unwanted_rows , inplace=True)
         df = df[df['polarity'] != 'polarity']
         
-        return df
+        return self.df
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
-        
+        self.df = df.drop_duplicates(subset='original_text')
         
         
         return df
+
+
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
+
+        df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
@@ -42,10 +44,11 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
+        df['polarity'] = pd.pd.to_numeric(df['polarity'], errors='coerce')
+
+        df['retweet_count'] = pd.to_numeric(df['retweet_count'], errors='coerce')
         
-        ----
-        ----
+        df['favourite_count'] = pd.to_numeric(df['favourite_count'], errors='coerce')
         
         return df
     
@@ -54,6 +57,7 @@ class Clean_Tweets:
         remove non english tweets from lang
         """
         
-        df = ----
+        df = df.query("lang == 'en' ")
         
         return df
+
